e0d497de8aa021587d9cfe43a8c38bb7425a71fc
ylose <heungeob1003@gmail.com>
2024-06-17 10:58:52 +0900

    Create Retrofit Instance

---
+++ app/src/main/java/com/wonchihyeon/yakbang/network/RetrofitInstance.kt
@@ -0,0 +1,23 @@
+package com.wonchihyeon.yakbang.network
+
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+object RetrofitInstance {
+    private const val BASE_URL = "http://apis.data.go.kr/1471000/DrbEasyDrugInfoService/"
+    private val retrofit by lazy {
+        Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .client(
+                OkHttpClient.Builder().addInterceptor(HttpLoggingInterceptor().apply {
+                    level = HttpLoggingInterceptor.Level.BODY
+                }).build()
+            )
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+    }
+
+    val service: YakService by lazy { retrofit.create(YakService::class.java) }
+}
