bf4ebf79133a0bbae2b8c1e5724f3b5d3072fe53
ylose <heungeob1003@gmail.com>
2024-06-17 11:35:20 +0900

    Relocate file and Add Viewmodel

--- app/src/main/java/com/wonchihyeon/yakbang/ui/home/HomeFragment.kt
+++ app/src/main/java/com/wonchihyeon/yakbang/ui/home/HomeFragment.kt
@@ -1,12 +1,16 @@
-package com.wonchihyeon.yakbang
+package com.wonchihyeon.yakbang.ui.home
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
+import com.wonchihyeon.yakbang.R
 import com.wonchihyeon.yakbang.databinding.FragmentFirstBinding
+import com.wonchihyeon.yakbang.repository.Category
 
 /**
  * A simple [Fragment] subclass as the default destination in the navigation.
@@ -18,6 +22,8 @@ class HomeFragment : Fragment() {
     // This property is only valid between onCreateView and
     // onDestroyView.
     private val binding get() = _binding!!
+
+    private val viewModel by viewModels<HomeViewModel>()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -32,8 +38,9 @@ class HomeFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        binding.buttonFirst.setOnClickListener {
-            findNavController().navigate(R.id.action_FirstFragment_to_SecondFragment)
+        viewModel.getYakList(Category.Efficacy, "감기")
+        viewModel.yakDataList.observe(viewLifecycleOwner) {
+            Log.d("YagBang", "list $it")
         }
     }
 
