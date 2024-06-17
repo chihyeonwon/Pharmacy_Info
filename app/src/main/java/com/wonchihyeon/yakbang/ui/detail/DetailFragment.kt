b34fa07ad36d19b95aca168caa8254084c1b4a1b
ylose <heungeob1003@gmail.com>
2024-06-17 11:34:51 +0900

    Relocate file

--- app/src/main/java/com/wonchihyeon/yakbang/ui/detail/DetailFragment.kt
+++ app/src/main/java/com/wonchihyeon/yakbang/ui/detail/DetailFragment.kt
@@ -1,4 +1,4 @@
-package com.wonchihyeon.yakbang
+package com.wonchihyeon.yakbang.ui.detail
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
@@ -6,6 +6,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.navigation.fragment.findNavController
+import com.wonchihyeon.yakbang.R
 import com.wonchihyeon.yakbang.databinding.FragmentSecondBinding
 
 /**
