2b3072d835c3273e9ed7137adb0e5641a42f4bbc
ylose <heungeob1003@gmail.com>
2024-06-18 20:38:16 +0900

    약 상세 페이지 구현

--- app/src/main/java/com/wonchihyeon/yakbang/ui/detail/DetailFragment.kt
+++ app/src/main/java/com/wonchihyeon/yakbang/ui/detail/DetailFragment.kt
@@ -1,40 +1,61 @@
 package com.wonchihyeon.yakbang.ui.detail
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.navigation.fragment.findNavController
+import androidx.fragment.app.Fragment
+import com.bumptech.glide.Glide
+import com.google.android.material.tabs.TabLayoutMediator
 import com.wonchihyeon.yakbang.R
+import com.wonchihyeon.yakbang.data.YakData
 import com.wonchihyeon.yakbang.databinding.FragmentDetailBinding
 
-/**
- * A simple [Fragment] subclass as the second destination in the navigation.
- */
 class DetailFragment : Fragment() {
 
     private var _binding: FragmentDetailBinding? = null
-
-    // This property is only valid between onCreateView and
-    // onDestroyView.
     private val binding get() = _binding!!
 
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
+        inflater: LayoutInflater,
+        container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-
+    ): View {
         _binding = FragmentDetailBinding.inflate(inflater, container, false)
         return binding.root
-
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
-        binding.buttonSecond.setOnClickListener {
-            findNavController().navigate(R.id.action_SecondFragment_to_FirstFragment)
+        with(binding) {
+            val detailList = arguments?.getParcelable<YakData>("yak_data")?.let {
+                Glide.with(this@DetailFragment).load(it.imageUrl)
+                    .placeholder(R.drawable.ic_open_yak).into(binding.yakIv)
+                yakNameTv.text = it.productName
+                yakCode.text = it.productCode
+                yakVendor.text = it.companyName
+                listOf(
+                    it.efficacy ?: "",
+                    it.interactions ?: "",
+                    it.precautions ?: "",
+                    it.storageMethod ?: ""
+                )
+            } ?: emptyList()
+            // 뷰페이저와 어댑터를 연결
+            viewPager.adapter = DetailAdapter(detailList)
+            backButton.setOnClickListener {
+                parentFragmentManager.popBackStack()
+            }
+            // 탭 레이아웃과 뷰페이지가 연동 되도록 연결
+            TabLayoutMediator(tabLayout, viewPager) { tab, position ->
+                tab.text = when (position) {
+                    0 -> "효능"
+                    1 -> "상호작용"
+                    2 -> "주의사항"
+                    3 -> "보관법"
+                    else -> null
+                }
+            }.attach()
         }
     }
 
